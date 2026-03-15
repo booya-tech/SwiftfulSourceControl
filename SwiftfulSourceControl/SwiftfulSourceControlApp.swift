87ee7da5ba59c1cc30cf054cb43752bc64734501
Panachai Sulsaksakul <panachais@phillip.co.th>
2026-03-15 20:59:29 +0700

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Panachai Sulsaksakul on 15/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
