f522a708bebe3a7759a239f7bf82ba5e9c29419f
Boo Pannachai <panachais@phillip.co.th>
2026-04-02 20:19:55 +0700

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Panachai Sulsaksakul on 4/2/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
